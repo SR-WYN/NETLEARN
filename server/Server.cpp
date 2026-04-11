c6b1fae5886145597786645342713c01d39f3266
wangyanan <2368495219@qq.com>
2026-04-11 14:14:21 +0800

    服务器优雅退出

--- server/Server.cpp
+++ server/Server.cpp
@@ -1,9 +1,10 @@
 #include "Server.hpp"
 #include "Session.hpp"
 #include <iostream>
+#include <csignal>
 using namespace std;
 
-Server::Server(boost::asio::io_context &ioc, short port) : _ioc(ioc), _acceptor(ioc, tcp::endpoint(tcp::v4(), port)) 
+Server::Server(boost::asio::io_context &ioc, short port) : _ioc(ioc), _acceptor(ioc, tcp::endpoint(tcp::v4(), port))
 {
     start_accept();
 }
@@ -15,19 +16,23 @@ void Server::ClearSession(std::string uuid)
 
 void Server::start_accept()
 {
-    shared_ptr<Session> new_session =  make_shared<Session>(_ioc,this);
-    _acceptor.async_accept(new_session->Socket(), 
-        [this, new_session](const boost::system::error_code& error) {
-            this->handle_accept(new_session, error);
-        }
-    );
+    shared_ptr<Session> new_session = make_shared<Session>(_ioc, this);
+    _acceptor.async_accept(new_session->Socket(),
+                           [this, new_session](const boost::system::error_code &error)
+                           {
+                               this->handle_accept(new_session, error);
+                           });
 }
 
-void Server::handle_accept(shared_ptr<Session> new_session, const boost::system::error_code& error) {
-    if (!error) {
+void Server::handle_accept(shared_ptr<Session> new_session, const boost::system::error_code &error)
+{
+    if (!error)
+    {
         new_session->Start();
-        _sessions.insert(make_pair(new_session->GetUuid(),new_session));
-    } else {
+        _sessions.insert(make_pair(new_session->GetUuid(), new_session));
+    }
+    else
+    {
 
         std::cout << "Accept error: " << error.message() << std::endl;
     }
@@ -39,12 +44,22 @@ int main()
     try
     {
         boost::asio::io_context ioc;
+        boost::asio::signal_set signals(ioc,SIGINT,SIGTERM);
+        signals.async_wait([&ioc](auto ec,auto sig_num){
+            if (ec)
+            {
+                cerr << "signal wait failed, error is " << ec.message() << endl;
+                return;
+            }
+            cout << "signal " << sig_num << " received, stopping server..." << endl;
+            ioc.stop();
+        });
         Server s(ioc,10086);
         ioc.run();
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
-        std::cerr <<"Exception: " << e.what() << '\n';
+        std::cerr << "Exception: " << e.what() << '\n';
     }
     return 0;
 }
